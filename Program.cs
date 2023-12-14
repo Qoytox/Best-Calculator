6f1a11bfec1dc60236fb22e865dfafa8b76a1a43
Qoytox <145270676+Qoytox@users.noreply.github.com>
2023-12-14 20:58:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("---------------------------------");
+Console.WriteLine("  Multiplier   Calculator   ");
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine("Please Enter Your 1rd Number: ");
+int No1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please Enter Your 2nd Number:");
+int No2 = Convert.ToInt32(Console.ReadLine());
+int Answer = No1 * No2;
+Console.WriteLine("Here is The Answer: " + Answer );
+
