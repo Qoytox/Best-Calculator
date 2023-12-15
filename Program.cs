5f22ccba849716765733c8a07787e1f0e66ec290
Qoytox <145270676+Qoytox@users.noreply.github.com>
2023-12-16 00:35:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,23 @@
 ﻿
 
-
+int No1;
 Console.BackgroundColor = ConsoleColor.DarkGreen;
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("---------------------------------");
 Console.WriteLine("  Multiplier   Calculator   ");
 Console.WriteLine("---------------------------------");
 
+Console.WriteLine("Please Enter Your Symbol: ");
+string sy = Console.ReadLine();
+
+if (op == +);
+{
+  int Answer = No1;
 Console.WriteLine("Please Enter Your 1rd Number: ");
 int No1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Please Enter Your 2nd Number:");
 int No2 = Convert.ToInt32(Console.ReadLine());
-int Answer = No1 * No2;
+
 Console.WriteLine("Here is The Answer: " + Answer );
 
