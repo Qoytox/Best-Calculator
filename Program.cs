457443e3abda319aef99396ec8a1755bb697d039
Qoytox <145270676+Qoytox@users.noreply.github.com>
2023-12-18 21:16:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
-﻿
 
-int No1;
+using System.Formats.Asn1;
 Console.BackgroundColor = ConsoleColor.DarkGreen;
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("---------------------------------");
-Console.WriteLine("  Multiplier   Calculator   ");
+Console.WriteLine("            Calculator           ");
 Console.WriteLine("---------------------------------");
 
 Console.WriteLine("Please Enter Your Symbol: ");
 string sy = Console.ReadLine();
-
-if (op == +);
-{
-  int Answer = No1;
+double Answer;
 Console.WriteLine("Please Enter Your 1rd Number: ");
-int No1 = Convert.ToInt32(Console.ReadLine());
+double No1 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("Please Enter Your 2nd Number:");
-int No2 = Convert.ToInt32(Console.ReadLine());
+double No2 = Convert.ToDouble(Console.ReadLine());
+if (sy == "+");
+{
+ double Answer = No1 + No2;
+}
 
-Console.WriteLine("Here is The Answer: " + Answer );
+if (sy == "-"){
+    double Answer = No1 - No2;
+}
+
+if (sy == "/");{
+ double Answer = No1 / No2;
+}
+
+if (sy == "*");{
+ double Answer = No1 * No2;
+}
+
+
+Console.WriteLine("Here is Your Answer: " + Answer);
+
 
